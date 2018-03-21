5a050d25db91941f28166322d728bafdc1e52f81
Dhairya <dhairyagada1997@gmail.com>
2018-03-21 23:36:56 +0530

    Reached Line 10 of Pseudocode

--- src.py
+++ src.py
@@ -1,9 +1,11 @@
 import numpy as np
+#from anytree import Node, RenderTree, AsciiStyle
+#import anytree
 
 gridMap = np.array([[1, 1, 1, 0, 0],[0, 0, 0, 0, 1],[0, 1, 1, 0, 0],[0, 0, 1, 1, 0],[1, 0, 0, 0, 0]]) 
 
-X=gridMap.shape[0]
-Y=gridMap.shape[1]
+X=gridMap.shape[0]-1
+Y=gridMap.shape[1]-1
 print(X)
 print(Y)
 ## Start Location 
@@ -32,6 +34,9 @@ def goalTest(currentrow,currentcol,endrow,endcol):
     # Testing if the current position is goal position
     return (currentrow==endrow and currentcol==endcol)
 
+
+    
+
 # Generating Succesors
 Succesors = lambda x, y : [(x2, y2) for x2 in range(x-1, x+2)
                                             for y2 in range(y-1, y+2)
@@ -45,31 +50,77 @@ Succesors = lambda x, y : [(x2, y2) for x2 in range(x-1, x+2)
 
 h=np.zeros((5,5))
 g=np.zeros((5,5))
+f=np.zeros((5,5))
+
 for i in range(5):
     for j in range(5):
         h[i,j]=heuristic(i,j,endrow,endcol)
         if gridMap[i,j]!=0:  
             g[i,j]=100 
-print(h)
-print(g)
 
-currentlocn=[startrow,startcol]         # Current Location Co-ordinates
-
-frontier_list=[currentlocn]             # List of Frontier Locations -- Visited But Not Explored
-
-explored_list=[]                        # List of Visted and Explored Locations
-
-f=np.zeros((5,5))                       # Total Cost Function
-
-f[startrow,startcol]=h[startrow,startcol]   # Initial Cost is Heuristic Cost
-while frontier_list!=0:
-    f[currentlocn[0],currentlocn[1]]= g[currentlocn[0],currentlocn[1]]+ h[currentlocn[0],currentlocn[1]]
-    if(goalTest(currentlocn[0],currentlocn[1],endrow,endcol)): break
-    next_loc=Succesors(currentlocn[0],currentlocn[1])
-    print(next_loc)
-    print(next_loc[0][0],next_loc[0][1])
-    print(f)
-    frontier_list=0
+    print(h)
+    print(g)
     
+    current_node=[startrow,startcol]         # Current Location Co-ordinates
+    
+    open_list=[current_node]             # List of Frontier Locations -- Visited But Not Explored
+    l=len(open_list)
+    print('Frontier',len(open_list))
+    explored_list=[]                        # List of Visted and Explored Locations
 
-                                   
+    f=np.zeros((5,5))                       # Total Cost Function
+   
+    f[startrow,startcol]=h[startrow,startcol]   # Initial Cost is Heuristic Cost
+    while open_list!=0:
+        print('Inside Loop')
+        print(open_list)
+        l=len(open_list)
+        minval = 1000
+        minrow=0
+        mincol=0
+        for i in range(l):
+            f[open_list[i][0],open_list[i][1]]=g[open_list[i][0],open_list[i][1]]+h[open_list[i][0],open_list[i][1]]
+            if minval>=f[open_list[i][0],open_list[i][1]]:
+                minval=f[open_list[i][0],open_list[i][1]]
+                minrow=open_list[i][0]
+                mincol=open_list[i][1]
+
+        current_node=[minrow,mincol]
+
+        if(goalTest(current_node[0],current_node[1],endrow,endcol)): break
+        
+        node_successor=Succesors(current_node[0],current_node[1])
+        nsl=len(node_successor)
+        for i in range(nsl):
+            succesor_cost=g[current_node[0],current_node[1]]+heuristic(current_node[0],current_node[1],
+            node_successor[i][0],node_successor[i][1])
+            if [node_successor[i][0],node_successor[i][1]] in open_list:
+                if g[node_successor[i][0],node_successor[i][1]] <=succesor_cost: print('WHERE TO GO?')
+        open_list=0 
+
+
+
+
+copy_succ=Succesors(2,4)
+print(copy_succ)
+copy_openlist=[[1,1],[1,2],[1,3],[1,4]]
+if[copy_succ[1][0],copy_succ[1][1]] in copy_openlist:
+    print('y')
+
+'''
+    nodex_open=[]
+    nodex_close=[]
+
+        nodex.append(Node([2,0]))
+        nodex.append(Node([1,2],parent=nodex[0]))
+        nodex.append(Node([0,1],parent=nodex[0]))
+        nodex.append(Node([0,2],parent=nodex[2],state="f"))
+        print(RenderTree(nodex[0]))
+        print(()!=anytree.search.findall_by_attr(nodex[0], name="state",value="f"))
+
+
+    nodex_open.append(Node([startrow,startcol]))
+    f[startrow,startcol]=g[startrow,startcol]
+    print([2,0]==nodex_open[0])
+    #while nodex_open!=[]:
+'''
